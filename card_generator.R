19e76dcf515a7237bfa776cfdab55eacf734346c
embruna <embruna@ufl.edu>
2023-10-09 23:45:47 -0400

    editing

--- card_generator.R
+++ card_generator.R
@@ -1,14 +1,15 @@
 # install.packages("devtools")
 # devtools::install_github("embruna/bingo", force=TRUE)
-
+# devtools::install_github("jennybc/bingo")
 library(bingo)
+library(here)
 
+here::i_am("card_generator.R")
 ## see some of the SuperBowl 50 squares
-tail(get_topic("football"))
+# tail(get_topic("football"))
+# tail(get_topic("jc2"))
+# tail(get_topic("math-proofs"))
 tail(get_topic("movie_junglecruise"))
-tail(get_topic("jc2"))
-tail(get_topic("math-proofs"))
-tail(get_topic("movie_congo"))
 #> [1] "Shot of Golden Gate Bridge"
 #> [2] "\"Silicon Valley and tech\" blah blah blah"
 #> [3] "Mike Carey is WRONG"
@@ -17,7 +18,7 @@ tail(get_topic("movie_congo"))
 #> [6] "Idle speculation it's Peyton's last game"
 
 ## make 8 bingo cards
-bc <- bingo(n_cards = 8, words = get_topic("movie_junglecruise"))
+bc <- bingo(n_cards = 80, words = get_topic("movie_junglecruise"))
 
 ## print them to PDF
 plot(bc)
